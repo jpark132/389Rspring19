394b48ad8a9344f7f2e10034d03d6c3601bb1196
jpark132 <andyjoonpark@gmail.com>
2019-04-13 05:25:18 -0400

    added finished c

--- assignments/7_Binaries_I/writeup/main.c
+++ assignments/7_Binaries_I/writeup/main.c
@@ -1,11 +1,33 @@
 /*
- * Name: *PUT YOUR NAME HERE*
- * Section: *PUT YOUR SECTION NUMBER HERE*
+ * Name: *Joon Park*
+ * Section: *0112*
  *
  * I pledge on my honor that I have not given or received any unauthorized
  * assistance on this assignment or examination.
  *
- * Digital acknowledgement: *PUT YOUR NAME HERE*
+ * Digital acknowledgement: *Joon park*
  */
 
 /* your code goes here */
+
+#include <stdio.h>
+
+int main()
+{
+
+   int a = 485163226; // icebooda
+	int b = 4277009102; //0xfeedface
+
+	printf("a = %d\n", a);
+	printf("b = %d\n", b);
+
+	//swaps a and b
+	int store = a;
+	a = b;
+	b = store;
+
+	printf("a = %d\n", a);
+	printf("b = %d\n", b);
+
+   return 0;
+}
