1016b55692c51e66b47db0022dababe5254b6752
jpark132 <andyjoonpark@gmail.com>
2019-04-24 22:38:46 -0400

    finished crackers

--- assignments/9_Crypto_I/writeup/crack.py
+++ assignments/9_Crypto_I/writeup/crack.py
@@ -4,15 +4,23 @@ import hashlib
 import string
 
 def crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+    hashes = open('hashes.txt','r').readlines()
+    passwords = open('passwords.txt','r').readlines()
     characters = string.ascii_lowercase
 
     for c in characters:
         for p in passwords:
-            # crack hashes
+            c=c.strip()
+            p=p.strip()
+            input = c+p
+            hash = hashlib.sha256(input.encode())
+            for h in hashes:
+                if(hash.hexdigest() == h.strip()):
+                    print(input+":"+hash.hexdigest())
 
-            # print hashes as 'input:hash'
+
+
+            # print hashes as 'input:hash
             # i.e.  yeet:909104cdb5b06af2606ed4a197b07d09d5ef9a4aad97780c2fe48053bce2be52
 
 if __name__ == "__main__":
--- assignments/9_Crypto_I/writeup/server_crack.py
+++ assignments/9_Crypto_I/writeup/server_crack.py
@@ -6,15 +6,29 @@ import socket
 import time
 
 def server_crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+    hashes = open('hashes.txt','r').readlines()
+    passwords = open('passwords.txt','r').readlines()
     characters = string.ascii_lowercase
-    server_ip = 'put_your_ip_here'
-    server_port = 00000
+    server_ip = '134.209.128.58'
+    server_port = 1337
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((server_ip, server_port))
-    data = s.recv(1024)
+    for x in range(3):
+        data = s.recv(1024)
+        data = data.splitlines()
+        data = data[2]
+        print(data)
+        for c in characters:
+            for p in passwords:
+                c = c.strip()
+                p = p.strip()
+                input = c+p
+                hash = hashlib.sha256(input.encode())
+                if hash.hexdigest().strip() == data.strip():
+                    print(input)
+                    time.sleep(5)
+                    s.send(input+"\n")
     # parse data
     # crack 3 times
 
